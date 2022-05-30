c7bdc67177282629ada5507f6e2fd713842e477c
jesslyn gabrielle <jesslyngh@yahoo.co.id>
2022-05-30 12:20:18 +0700

    Add project files.

---
+++ praktikumw14/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace praktikumw14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelManager = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelTeam = new System.Windows.Forms.Label();
+            this.labelStadium = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelTop = new System.Windows.Forms.Label();
+            this.labelWorst = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonDPrev = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonDNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Team Name :";
+            // 
+            // labelManager
+            // 
+            this.labelManager.AutoSize = true;
+            this.labelManager.Location = new System.Drawing.Point(259, 271);
+            this.labelManager.Name = "labelManager";
+            this.labelManager.Size = new System.Drawing.Size(0, 25);
+            this.labelManager.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Manager :";
+            // 
+            // labelTeam
+            // 
+            this.labelTeam.AutoSize = true;
+            this.labelTeam.Location = new System.Drawing.Point(259, 205);
+            this.labelTeam.Name = "labelTeam";
+            this.labelTeam.Size = new System.Drawing.Size(0, 25);
+            this.labelTeam.TabIndex = 0;
+            // 
+            // labelStadium
+            // 
+            this.labelStadium.AutoSize = true;
+            this.labelStadium.Location = new System.Drawing.Point(259, 334);
+            this.labelStadium.Name = "labelStadium";
+            this.labelStadium.Size = new System.Drawing.Size(0, 25);
+            this.labelStadium.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Stadium :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Top Scorer :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 456);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Worst Discipline :";
+            // 
+            // labelTop
+            // 
+            this.labelTop.AutoSize = true;
+            this.labelTop.Location = new System.Drawing.Point(259, 394);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(0, 25);
+            this.labelTop.TabIndex = 0;
+            // 
+            // labelWorst
+            // 
+            this.labelWorst.AutoSize = true;
+            this.labelWorst.Location = new System.Drawing.Point(259, 456);
+            this.labelWorst.Name = "labelWorst";
+            this.labelWorst.Size = new System.Drawing.Size(0, 25);
+            this.labelWorst.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 550);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(907, 492);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonDPrev
+            // 
+            this.buttonDPrev.Location = new System.Drawing.Point(70, 60);
+            this.buttonDPrev.Name = "buttonDPrev";
+            this.buttonDPrev.Size = new System.Drawing.Size(99, 73);
+            this.buttonDPrev.TabIndex = 2;
+            this.buttonDPrev.Text = "<<";
+            this.buttonDPrev.UseVisualStyleBackColor = true;
+            this.buttonDPrev.Click += new System.EventHandler(this.buttonDPrev_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(233, 60);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(99, 73);
+            this.buttonPrev.TabIndex = 2;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(403, 60);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(99, 73);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonDNext
+            // 
+            this.buttonDNext.Location = new System.Drawing.Point(584, 60);
+            this.buttonDNext.Name = "buttonDNext";
+            this.buttonDNext.Size = new System.Drawing.Size(99, 73);
+            this.buttonDNext.TabIndex = 2;
+            this.buttonDNext.Text = ">>";
+            this.buttonDNext.UseVisualStyleBackColor = true;
+            this.buttonDNext.Click += new System.EventHandler(this.buttonDNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 1234);
+            this.Controls.Add(this.buttonDNext);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonDPrev);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelTeam);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelManager);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelWorst);
+            this.Controls.Add(this.labelTop);
+            this.Controls.Add(this.labelStadium);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelManager;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelTeam;
+        private System.Windows.Forms.Label labelStadium;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelTop;
+        private System.Windows.Forms.Label labelWorst;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonDPrev;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonDNext;
+    }
+}
+
---
+++ praktikumw14/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace praktikumw14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string sqlConnection = "server=139.255.11.84;uid=student;pwd=isbmantap;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        string sqlQuery;
+        int counter = 0;
+        string temp = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            sqlQuery = "SELECT `team_name`, CONCAT(manager_name , '(' , n.nation , ')') as Manager , CONCAT(home_stadium , ', ' , city , '(' , capacity , ')') as Stadium, (j.jumlah_goal + j.jumlah_goal_penalty) as Top_Scorer, concat(`player_name`,'. ',(j.jumlah_kartu_kuning),' Yellow Card and ',j.jumlah_kartu_merah,' Red Card') AS 'Worst Discipline' FROM team t, manager m, nationality n, dmatch d, dt_Jesslyn j WHERE t.manager_id = m.manager_id and t.team_id = d.team_id AND d.player_id = j.player_id and m.nationality_id = n.nationality_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            sqlAdapter.Fill(dt);
+            labelTeam.Text = dt.Rows[0][0].ToString();
+            labelManager.Text = dt.Rows[0][1].ToString();
+            labelStadium.Text = dt.Rows[0][2].ToString();
+            labelTop.Text = dt.Rows[0][3].ToString();
+            labelWorst.Text = dt.Rows[0][4].ToString();
+
+            sqlQuery = "select match_date, date_format(m.match_date, \"%d/%c/%Y\") as `Match Date`, 'HOME' as `Home/Away`, concat('vs ', t.team_name) as `Lawan`, concat(m.goal_home, ' - ', m.goal_away) as `Score` from `match` m, team t where m.team_home = 'A001' and m.team_away = t.team_id union select match_date, date_format(m.match_date, \"%d/%c/%Y\") as `Match Date`, 'AWAY' as 'Home/Away', concat('vs ', t.team_name) as Lawan, concat(m.goal_home, ' - ', m.goal_away) as Score from `match` m, team t where m.team_away = 'A001' and m.team_home = t.team_id order by 1 desc limit 0, 5;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable lastMatch = new DataTable();
+            sqlAdapter.Fill(lastMatch);
+            dataGridView1.DataSource = lastMatch;
+        }
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (counter <= 0)
+            {
+                MessageBox.Show("Ini Data Pertama");
+            }
+            else
+            {
+                counter--;
+                sqlQuery = "SELECT `team_name`, CONCAT(manager_name , '(' , n.nation , ')') as Manager , CONCAT(home_stadium , ', ' , city , '(' , capacity , ')') as Stadium, (j.jumlah_goal + j.jumlah_goal_penalty) as Top_Scorer, concat(`player_name`,'. ',(j.jumlah_kartu_kuning),' Yellow Card and ',j.jumlah_kartu_merah,' Red Card') AS 'Worst Discipline' FROM team t, manager m, nationality n, dmatch d, dt_Jesslyn j WHERE t.manager_id = m.manager_id and t.team_id = d.team_id AND d.player_id = j.player_id and m.nationality_id = n.nationality_id";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                DataTable Prev = new DataTable();
+                sqlAdapter.Fill(Prev);
+                labelTeam.Text = Prev.Rows[counter][0].ToString();
+                labelManager.Text = Prev.Rows[counter][1].ToString();
+                labelStadium.Text = Prev.Rows[counter][2].ToString();
+                labelTop.Text = Prev.Rows[counter][3].ToString();
+                labelWorst.Text = Prev.Rows[counter][4].ToString();
+            }
+        }
+
+        private void buttonDPrev_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            sqlQuery = "SELECT `team_name`, CONCAT(manager_name , '(' , n.nation , ')') as Manager , CONCAT(home_stadium , ', ' , city , '(' , capacity , ')') as Stadium, (j.jumlah_goal + j.jumlah_goal_penalty) as Top_Scorer, concat(`player_name`,'. ',(j.jumlah_kartu_kuning),' Yellow Card and ',j.jumlah_kartu_merah,' Red Card') AS 'Worst Discipline' FROM team t, manager m, nationality n, dmatch d, dt_Jesslyn j WHERE t.manager_id = m.manager_id and t.team_id = d.team_id AND d.player_id = j.player_id and m.nationality_id = n.nationality_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable DPrev = new DataTable();
+            sqlAdapter.Fill(DPrev);
+            labelTeam.Text = DPrev.Rows[counter][0].ToString();
+            labelManager.Text = DPrev.Rows[counter][1].ToString();
+            labelStadium.Text = DPrev.Rows[counter][2].ToString();
+            labelTop.Text = DPrev.Rows[counter][3].ToString();
+            labelWorst.Text = DPrev.Rows[counter][4].ToString();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (counter >= 19)
+            {
+                MessageBox.Show("Ini Data Terakhir");
+            }
+            else
+                counter++;
+            sqlQuery = "SELECT `team_name`, CONCAT(manager_name , '(' , n.nation , ')') as Manager , CONCAT(home_stadium , ', ' , city , '(' , capacity , ')') as Stadium, (j.jumlah_goal + j.jumlah_goal_penalty) as Top_Scorer, concat(`player_name`,'. ',(j.jumlah_kartu_kuning),' Yellow Card and ',j.jumlah_kartu_merah,' Red Card') AS 'Worst Discipline' FROM team t, manager m, nationality n, dmatch d, dt_Jesslyn j WHERE t.manager_id = m.manager_id and t.team_id = d.team_id AND d.player_id = j.player_id and m.nationality_id = n.nationality_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable Next = new DataTable();
+            sqlAdapter.Fill(Next);
+            labelTeam.Text = Next.Rows[counter][0].ToString();
+            labelManager.Text = Next.Rows[counter][1].ToString();
+            labelStadium.Text = Next.Rows[counter][2].ToString();
+            labelTop.Text = Next.Rows[counter][3].ToString();
+            labelWorst.Text = Next.Rows[counter][4].ToString();
+        }
+
+        private void buttonDNext_Click(object sender, EventArgs e)
+        {
+            counter = 19;
+            sqlQuery = "SELECT `team_name`, CONCAT(manager_name , '(' , n.nation , ')') as Manager , CONCAT(home_stadium , ', ' , city , '(' , capacity , ')') as Stadium, (j.jumlah_goal + j.jumlah_goal_penalty) as Top_Scorer, concat(`player_name`,'. ',(j.jumlah_kartu_kuning),' Yellow Card and ',j.jumlah_kartu_merah,' Red Card') AS 'Worst Discipline' FROM team t, manager m, nationality n, dmatch d, dt_Jesslyn j WHERE t.manager_id = m.manager_id and t.team_id = d.team_id AND d.player_id = j.player_id and m.nationality_id = n.nationality_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable Next = new DataTable();
+            sqlAdapter.Fill(Next);
+            labelTeam.Text = Next.Rows[counter][0].ToString();
+            labelManager.Text = Next.Rows[counter][1].ToString();
+            labelStadium.Text = Next.Rows[counter][2].ToString();
+            labelTop.Text = Next.Rows[counter][3].ToString();
+            labelWorst.Text = Next.Rows[counter][4].ToString();
+        }
+    }
+}
